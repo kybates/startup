ac816aaba5b61ea7c11121e1988ad7097a753f6d
kybates <hulmek@byu.edu>
2023-03-09 14:38:18 -0700

    Created addRecipeName(), linked add.js to add.html

---
+++ add.js
@@ -0,0 +1,17 @@
+function addRecipeName() {
+    const recipeNameEl = document.querySelector('#recipeName');
+    const recipeName = document.getElementById('text-input').value;
+    recipeNameEl.innerHTML = `<h2>${recipeName}</h2>`;
+}
+
+function addIngredients() {
+
+}
+
+function addDirections() {
+
+}
+
+function createRecipe() {
+
+}
