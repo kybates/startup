03427362d1bfd3a875c21eca5d1b81b6829000f7
kybates <hulmek@byu.edu>
2023-04-03 19:34:37 -0600

    Created service index.js, added deployService.sh

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// Serve up the application's static content
+app.use(express.static('public'));
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
