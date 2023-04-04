b04aa1bac40552fb4cff1c963f8193bef54aa4ea
kybates <hulmek@byu.edu>
2023-04-03 19:57:24 -0600

    Added database.js

---
+++ database.js
@@ -0,0 +1,13 @@
+const {MongoClient} = require('mongodb');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 // The service port. In production the application is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
 // Serve up the application's static content
 app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);     
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
